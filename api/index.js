05d1445060e16d6309c2e316858f0d07e812e869
Sandesh Kolhe <sandykolhe98@gmail.com>
2024-06-28 02:08:37 +0530

    db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/blog-posts').then(()=>{
+    console.log("connected to db");
+
+}).catch((err)=>{
+    console.log(err);
+})
+
 
 app.listen(3000,()=>{
     console.log('server running on port 3000')
