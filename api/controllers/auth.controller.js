299507b7af31373c2a7f7278e5f27c1341f66348
Sandesh Kolhe <sandykolhe98@gmail.com>
2024-06-28 09:26:09 +0530

    added error handler

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import User from '../models/users.models.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async (req, res,next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    // return res.status(400).json({ message: 'All fields are required' });
+    next(errorHandler(400, 'All fields are required'));
+  
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    // res.status(500).json({ message: error.message });
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+// import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.routes.js'
+
 const app = express();
+dotenv.config();
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/blog-posts').then(()=>{
     console.log("connected to db");
@@ -13,3 +19,15 @@ mongoose.connect('mongodb://localhost:27017/blog-posts').then(()=>{
 app.listen(3000,()=>{
     console.log('server running on port 3000')
 })
+
+// app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  })
